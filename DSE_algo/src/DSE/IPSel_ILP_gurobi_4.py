6f417e88b7b5cf70f9ce34525fa2673b2b8bebc6
Wei Zuo <zuowei880227@gmail.com>
2019-02-02 14:29:23 -0600

    finished the skeleton

--- DSE_algo/src/DSE/IPSel_ILP_gurobi_4.py
+++ DSE_algo/src/DSE/IPSel_ILP_gurobi_4.py
@@ -146,43 +146,9 @@ class IPSel():
 
         print "Final latency_achieved", lat_achieved
 
-        #THIS IS JUST FOR DEBUGGING
-        for g in mapping_solution:
-            for n in list(g.nodes):
-                if n.type not in hw_layers:
-                    g.remove_node(n)
-        assignOriginalNodeMapping(mapping_solution, hw_layers)
+        #After the is done, re-order the mapping
+        reorderMapping(mapping_solution, hw_layers)
 
-        convIPs = set()
-        for g in mapping_solution:
-            for n in g:
-                if n.type is "combineNode":
-                    for m in n.node_list:
-                        if m.type == "Convolution":
-                            convIPs.add(m.mappedIP)
-                if n.type == "Convolution":
-                    convIPs.add(n.mappedIP)
-        convIPs = list(convIPs)
-        print "CONVIPS", convIPs
-        for g in mapping_solution:
-            idx = 0
-            for n in mapping_solution[g]:
-                if n.type is "combineNode":
-                    for m in n.node_list:
-                        if m.type == "Convolution":
-                            m.mappedIP = convIPs[idx]
-                            idx += 1
-                if n.type == "Convolution":
-                    n.mappedIP = convIPs[idx]
-                    idx += 1
-        #########
-
-        for g in mapping_solution:
-            for n in g.nodes:
-                print "abcdedf", n.name
-            print gs.printNodesMapping(hw_layers, mapping_solution[g])
-
-        #Code Generation Phase
         IP_g = createIPGraph(mapping_solution, hw_layers)
         expandGraph(IP_g)
         muxSelTable = assignMuxSelTable(IP_g)
@@ -257,3 +223,53 @@ class IPSel():
     def updateLayerQueue(self, layerQueueIn, layerQueueOut):
         for layer_type in layerQueueIn:
             layerQueueOut += layerQueueIn[layer_type]
+
+
+def reorderMapping(mapping_solution, hw_layers):
+    #For the solution, for each IP collect the mapping, 
+    #reassign using the best order
+    for g in mapping_solution:
+        for n in list(g.nodes):
+            if n.type not in hw_layers:
+                g.remove_node(n)
+
+    assignOriginalNodeMapping(mapping_solution, hw_layers)
+
+    #Collect the set of IPs for each IP ID
+    IPs = dict()
+    IPsIdx =  dict()
+    for g in mapping_solution:
+        for n in g:
+            if n.type is "combineNode":
+                for m in n.node_list:
+                    if m.type in hw_layers:
+                        IPName = m.mappedIP.name.split("_")[0]
+                        IPs[IPName] = [m.mappedIP] if IPName not in IPs else (IPs[IPName] + [m.mappedIP])
+            if n.type in hw_layers:
+                if n.type in hw_layers:
+                    IPName = n.mappedIP.name.split("_")[0]
+                    IPs[IPName] = [n.mappedIP] if IPName not in IPs else (IPs[IPName] + [n.mappedIP])
+
+    #initialize idx
+    for IPName in IPs:
+        IPsIdx[IPName] = 0
+
+    #Reassign IP
+    for g in mapping_solution:
+        #clear idx
+        for IPName in IPsIdx:
+            IPsIdx[IPName] = 0
+
+        for n in mapping_solution[g]:
+            if n.type is "combineNode":
+                for m in n.node_list:
+                    if m.type in hw_layers:
+                        IPName = m.mappedIP.name.split("_")[0]
+                        m.mappedIP = IPs[IPName][IPsIdx[IPName]]
+                        IPsIdx[IPName] += 1
+            if n.type in hw_layers:
+                IPName = n.mappedIP.name.split("_")[0]
+                n.mappedIP = IPs[IPName][IPsIdx[IPName]]
+                IPsIdx[IPName] += 1
+
+
