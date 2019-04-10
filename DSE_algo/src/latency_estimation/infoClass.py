7139311a2f62b8d8d97eb7799c64df1c750370f7
xliu79 <greatheng79@gmail.com>
2019-04-09 22:29:42 -0500

    add self in the class

--- DSE_algo/src/latency_estimation/infoClass.py
+++ DSE_algo/src/latency_estimation/infoClass.py
@@ -1,58 +1,58 @@
 class layerInfo_t():
-    layerType=None
-    inp_height=None
-    inp_width=None
-    out_height=None
-    out_width=None
-    out_planes=None
-    inp_planes=None
-    stride=None
-    filter_height=None
-    filter_width=None
-    pad=None
-    groupFlag=None
-    layerID=None
-    memIn=None
-    memInL=None
-    memInR=None
-    memOut=None
-    rowStep=None
+    self.layerType=None
+    self.inp_height=None
+    self.inp_width=None
+    self.out_height=None
+    self.out_width=None
+    self.out_planes=None
+    self.inp_planes=None
+    self.stride=None
+    self.filter_height=None
+    self.filter_width=None
+    self.pad=None
+    self.groupFlag=None
+    self.layerID=None
+    self.memIn=None
+    self.memInL=None
+    self.memInR=None
+    self.memOut=None
+    self.rowStep=None
 
 class IPinfo_t():
-    IPtype=None
-    IPidx=None 
-    K_x_P= None
-    XI_KER_PROC=None
-    XI_PIX_PROC=None
-    XI_WEIGHTBUFF_DEPTH=None
-    XI_INDEPTH=None;
-    XI_OUTDEPTH=None;
-    int6bit=None
-    BRAM=None
-    IBRAM=None
-    OBRAM=None
-    WBRAM=None
-    OtherBRAM=None
+    self.IPtype=None
+    self.IPidx=None 
+    self.K_x_P= None
+    self.XI_KER_PROC=None
+    self.XI_PIX_PROC=None
+    self.XI_WEIGHTBUFF_DEPTH=None
+    self.XI_INDEPTH=None;
+    self.XI_OUTDEPTH=None;
+    self.int6bit=None
+    self.BRAM=None
+    self.IBRAM=None
+    self.OBRAM=None
+    self.WBRAM=None
+    self.OtherBRAM=None
 
 
 class runInfo_t():
     #note by XInheng: I am trying to simulate latency for rounds that involves multiple pipeline chain
     #latency for multiple pipeline chain would be not accurate
     #we current cannot support branch structure
-    layerInfo=None #layerInfo_t
-    IPidx=None
-    nextIPidx=None #if it is involved in a one chain pipeline, then specify next IPidx
-    prevIPidx=None #if it is involved in a one chain pipeline, then specify prev IPidx
+    self.layerInfo=None #layerInfo_t
+    self.IPidx=None
+    self.nextIPidx=None #if it is involved in a one chain pipeline, then specify next IPidx
+    self.prevIPidx=None #if it is involved in a one chain pipeline, then specify prev IPidx
     
 
 
 
 class roundILPInfo_t():
-    roundIdx=None;
-    rowStep=None;
-    IBRAMList=[];
-    OBRAMList=[];
-    IPindexList=[];
-    ConstantBRAM;
+    self.roundIdx=None;
+    self.rowStep=None;
+    self.IBRAMList=[];
+    self.OBRAMList=[];
+    self.IPindexList=[];
+    self.ConstantBRAM;
 
 
